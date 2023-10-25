c9cdd456838cf8ac1639bb31ca3dece68153ed90
mehmetaziz <94168775+mehmetaziz@users.noreply.github.com>
2023-10-25 23:06:11 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  String ilce = "Çorlu";
+
+  print(ilce);
+
+  String kita = "Avrupa";
+
+  print(kita);
+
+  int faksNo = 08504202214;
+
+  print(faksNo);
+
+  int postaKodu = 59840;
+
+  print(postaKodu);
+
+  String insta = "https://www.instagram.com/mehmetaziz59/";
+
+  print(insta);
+
+  String calisilanBolum = "Bilgi İşlem";
+
+  print(calisilanBolum);
+
+  int urunMiktari = 25;
+
+  print("Miktar : $urunMiktari");
+
+  String musteriSoyad = "Turan";
+
+  print("Müşteri Soyadı : $musteriSoyad");
+
+  int odemeMiktari = 125;
+
+  print("Ödeme Miktarı : $odemeMiktari");
+
+  DateTime dogumTarihi = DateTime(2002, 2, 23);
+
+  print("Doğum Tarihi :$dogumTarihi ");
+
+  int borc = 1500;
+
+  print("Borç: $borc");
+
+//bekar ise true evli ise false
+  bool medeniHal = true;
+
+  print(medeniHal);
+
+  String yorum = "İlk yorum";
+
+  print(yorum);
+
+  DateTime odemeSaati = DateTime.now();
+
+  print(
+      "Ödeme Saati:${odemeSaati.hour}.${odemeSaati.minute}.${odemeSaati.second}");
+
+  int eftMiktar = 5000;
+
+  print("Gönderilen Miktar: $eftMiktar");
+
+  int satilanMiktar = 3;
+
+  print("Bugün satılan ürün sayısı: $satilanMiktar");
+
+  String telefonModeli = "S23";
+
+  print("Telefon Modeli: $telefonModeli");
+
+  String dergiAdi = "F1TR";
+
+  print("Dergi Adı: $dergiAdi");
+
+  DateTime basimTarihi = DateTime(2017, 9, 21);
+
+  print("Basım Tarihi :$basimTarihi ");
+
+  int zam = 20;
+
+  print("Uygulanan Zam: %$zam");
+
+  int daireSayisi = 4;
+
+  print("Daire Sayısı: $daireSayisi");
+
+  double enlem = 48.5;
+
+  print("Enlem: $enlem");
+
+  double boylam = 28.4;
+
+  print("Boylam: $boylam");
+
+  String yemekAdi = "Baniçka";
+
+  print(yemekAdi);
+
+  double fiyat = 259.99;
+
+  print("Fiyat: $fiyat");
+
+  String sirketAdi = "Mehmet Tic.";
+
+  print("Şirket Adı: $sirketAdi");
+
+  String videoAdi = "Flutter 101";
+
+  print("Video Adı: $videoAdi");
+
+  double muzikSuresi = 3.17;
+
+  print("Müzik Süresi: $muzikSuresi");
+
+  double mekanPuani = 4.5;
+
+  print("Puan: $mekanPuani");
+
+  String dosyaAdi = "readme.xps";
+
+  print("Dosya Adı:$dosyaAdi");
+
+  String resimFormati = "jpeg";
+
+  print("Resmin Formatı: $resimFormati");
+
+  String renk = "Mavi";
+
+  print(renk);
+
+  String renkKodu = "0.0.255";
+
+  print(renkKodu);
+
+  String bilgisayarModeli = "81Y4";
+
+  print("Bilgisayar Modeli: $bilgisayarModeli");
+
+  double ekranBoyutu = 15.6;
+
+  print("Ekran Boyutu: $ekranBoyutu");
+
+  String kullanimSuresi = "2 Saat";
+
+  print("Kullanım Süresi: $kullanimSuresi");
+
+  int bar = 1;
+
+  print("Basınç: $bar Bar");
+
+  DateTime etkinlikGunu = DateTime(2023, 10, 26);
+
+  print("Etkinlik Tarihi: $etkinlikGunu");
+
+  DateTime odemeGunu = DateTime(2023, 10, 30);
+
+  print("Ödeme Tarihi: $odemeGunu");
+
+  DateTime yolculukGunu = DateTime(2023, 12, 13);
+
+  print("Yolculuk Çıkış Tarihi: $yolculukGunu");
+
+  String mahalle = "Cemaliye";
+
+  print("Mahalle Adı: $mahalle");
+
+  String hat = "7-11";
+
+  print("Hat: $hat");
+
+  int kullanilanDakika = 15;
+
+  print("Kullanılan Dakika: $kullanilanDakika");
+
+  int kargoTakipNo = 366542;
+
+  print("Takip No: $kargoTakipNo");
+
+  String kuponSuresi = "15 Gün";
+
+  print("Kupon Süresi: $kuponSuresi");
+
+  int kuponKod = 15447856;
+
+  print("Kupon Kodu: $kuponKod");
+
+   DateTime faturaTarihi = DateTime(2023, 10, 09);
+
+  print("Fatura Tarihi: $faturaTarihi");
+  
+}
